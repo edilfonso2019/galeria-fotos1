339838be44485487a639ebda15782af59895d3f6
edilfonso2019 <benjaminedilfonso@gmail.com>
2022-11-10 17:58:32 -0500

    onclick

---
+++ script.js
@@ -0,0 +1,11 @@
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference;
+}
+
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
