ae1132e530504614143b4aa7a971abae68a6ef62
edilfonso2019 <benjaminedilfonso@gmail.com>
2022-11-10 20:55:18 -0500

    agregue onclick y evento teclado para salir de fotografia grande y volver al menu

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function openFulImg(reference) {
 function closeImg() {
   fulImgBox.style.display = "none";
 }
+
+document.body.addEventListener("keydown", function (event) {
+  console.log(event.code, event.keyCode);
+  if (event.code === "Escape" || event.keyCode === 27) {
+    // Aqui la lógica para el caso de Escape ...
+    fulImgBox.style.display = "none";
+  }
+});
