a3b50660e0dca3a69a300fc89dc97256638dcdd9
igor <igor.ramos.c@hotmail.com>
2022-11-10 19:57:53 -0500

    repare el problemas de las imagenes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const fulImgBox = document.getElementById("fulImgBox"),
-fulImg = document.getElementById("fulImg");
+  fulImg = document.getElementById("fulImg");
 
-function openFulImg(reference){
-    fulImgBox.style.display = "flex";
-    fulImg.src = reference;
+function openFulImg(reference) {
+  fulImgBox.style.display = "flex";
+  fulImg.src = reference.firstElementChild.src;
 }
 
-function closeImg(){
-    fulImgBox.style.display = "none";
+function closeImg() {
+  fulImgBox.style.display = "none";
 }
